d2d5ea8a30ae8d3b896973f83d2db93438d92126
Andrew Hall <andrewmartinhall2@gmail.com>
2024-01-27 18:16:58 -0700

    Car Info page

--- app/(tabs)/carInfo.jsx
+++ app/(tabs)/carInfo.jsx
@@ -8,18 +8,28 @@ export default function TabOneScreen() {
     make: "Toyota",
     model: "Corolla",
     year: 2010,
-    licensePlate: "MGB-2908",
+    licensePlate: "MGB-2909",
     expires: "4/5/2023",
   };
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        {carInfo.make} {carInfo.model}
-      </Text>
-      <Text>License Plate: {carInfo.licensePlate}</Text>
-      <Text>Valid Through: {carInfo.expires}</Text>
-
-      <View flex row style={{ position: "absolute", bottom: 50 }}>
+    <View flex style={styles.container}>
+      <View>
+        <View style={{backgroundColor: '#d3d3d3'}}>
+          <Text style={{padding: 10}}>
+            Active Vehicles
+          </Text>
+        </View>
+        <View style={{backgroundColor: '#90EE90'}}>
+          <View style={{padding: 10}}>
+            <Text style={styles.title}>
+              {carInfo.make} {carInfo.model}
+            </Text>
+            <Text>License Plate: {carInfo.licensePlate}</Text>
+            <Text>Valid Through: {carInfo.expires}</Text>
+          </View>
+        </View>
+      </View>
+      <View flex row style={{ position: "absolute", bottom: 50, width: '100%', justifyContent: 'center' }}>
         <Button label="Add Car" />
       </View>
     </View>
@@ -28,9 +38,7 @@ export default function TabOneScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+
   },
   title: {
     fontSize: 20,
@@ -41,4 +49,8 @@ const styles = StyleSheet.create({
     height: 1,
     width: "80%",
   },
+  car: {
+    backgroundColor: 'red',
+    marginLeft: '100px',
+  }
 });
